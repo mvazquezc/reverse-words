84b0c0e12ea67972217f15134c3c3ab2cd4b079d
root <root@ip-10-0-0-34.ec2.internal>
2020-07-01 12:00:39 +0000

    Release updated to v0.0.8

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ var (
 	)
 )
 
-var version = "v0.0.7"
+var version = "v0.0.8"
 
 // ReturnRelease returns the release configured by the user
 func ReturnRelease(w http.ResponseWriter, r *http.Request) {
