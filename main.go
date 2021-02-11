a8a51732295a77f771c5b0a7277e8551c3d99e44
Mario Vazquez <mavazque@redhat.com>
2021-02-11 10:59:22 +0100

    Release updated to v0.0.20

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ var (
 	)
 )
 
-var version = "v0.0.19"
+var version = "v0.0.20"
 
 // ReturnRelease returns the release configured by the user
 func ReturnRelease(w http.ResponseWriter, r *http.Request) {
