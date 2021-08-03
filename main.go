4c1945d3d12937ebfd461686ffe22fba5f7c91f4
sleclercs <sleclercs@gmail.com>
2021-08-03 11:59:29 -0400

    Update main.go

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ var (
 	)
 )
 
-var version = "v0.0.30"
+var version = "v0.0.31"
 
 // ReturnRelease returns the release configured by the user
 func ReturnRelease(w http.ResponseWriter, r *http.Request) {
